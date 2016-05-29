adda28abf87332d53b2367d111c683dfafdf1730
Lazbaj <lazbaj@gmail.com>
2016-05-29 13:47:48 +0200

    Add check_identity_matrix

---
+++ check_list_symmetry-identity_matrix.py
@@ -0,0 +1,120 @@
+# A list is symmetric if the first row is the same as the first column,
+# the second row is the same as the second column and so on. Write a
+# procedure, symmetric, which takes a list as input, and returns the
+# boolean True if the list is symmetric and False if it is not.
+def symmetric(p):
+	i=0
+	while i<len(p):
+		j=0
+		if len(p[i]) != len(p):
+			return False
+		while j<len(p):
+			if p[i][j] != p[j][i]:
+				return False
+			j = j+1
+		i = i+1
+	return True
+
+print symmetric([[1, 2, 3],
+                [2, 3, 4],
+                [3, 4, 1]])
+#>>> True
+
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "fish"],
+                ["fish", "fish", "cat"]])
+#>>> True
+
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "dog"],
+                ["fish","fish","cat"]])
+#>>> False
+
+print symmetric([[1, 2],
+                [2, 1]])
+#>>> True
+
+print symmetric([[1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [3, 4, 5, 6]])
+#>>> False
+
+print symmetric([[1,2,3],
+                 [2,3,1]])
+#>>> False
+
+
+# By Ashwath from forums
+# Given a list of lists representing a n * n matrix as input, 
+# define a  procedure that returns True if the input is an identity matrix 
+# and False otherwise.
+
+# An IDENTITY matrix is a square matrix in which all the elements 
+# on the principal/main diagonal are 1 and all the elements outside 
+# the principal diagonal are 0. 
+# (A square matrix is a matrix in which the number of rows 
+# is equal to the number of columns)
+
+def is_identity_matrix(p):
+	i=0
+	while i<len(p):
+		j=0
+		if len(p[i]) != len(p):
+			return False
+		while j<len(p):
+			if (i==j and p[i][j]!=1) or (i!=j and p[i][j]!=0):
+				return False
+			j = j+1
+		i = i+1
+	return True
+
+
+
+# Test Cases:
+
+matrix1 = [[1,0,0,0],
+           [0,1,0,0],
+           [0,0,1,0],
+           [0,0,0,1]]
+print is_identity_matrix(matrix1)
+#>>>True
+
+matrix2 = [[1,0,0],
+           [0,1,0],
+           [0,0,0]]
+
+print is_identity_matrix(matrix2)
+#>>>False
+
+matrix3 = [[2,0,0],
+           [0,2,0],
+           [0,0,2]]
+
+print is_identity_matrix(matrix3)
+#>>>False
+
+matrix4 = [[1,0,0,0],
+           [0,1,1,0],
+           [0,0,0,1]]
+
+print is_identity_matrix(matrix4)
+#>>>False
+
+matrix5 = [[1,0,0,0,0,0,0,0,0]]
+
+print is_identity_matrix(matrix5)
+#>>>False
+
+matrix6 = [[1,0,0,0],  
+           [0,1,0,1],  
+           [0,0,1,0],  
+           [0,0,0,1]]
+
+print is_identity_matrix(matrix6)
+#>>>False
+
+matrix7 = [[1, -1, 1],
+           [0, 1, 0],
+           [0, 0, 1]]
+print is_identity_matrix(matrix7)
+#>>>False  
